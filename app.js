0e4302f4f6a6ba22b2e10ead00dc5b6dc2673398
nickanyi-dev <nickanyi.dev@gmail.com>
2023-08-03 13:19:40 +0300

    function to determine winner

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const options = ["rock", "paper", "scissors"]
+const options = ["rock", "paper", "scissors"];
 
-
-function getComputerChoice(){
-    const choice = options[Math.floor(Math.random() * options.length)];
-   console.log(choice)
+function getComputerChoice() {
+  const choice = options[Math.floor(Math.random() * options.length)];
+  console.log(choice);
+  return choice;
 }
 
-getComputerChoice();
+function checkWinner(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "Tie";
+  } else if( (playerSelection == "rock" && computerSelection == "scissors") || 
+  (playerSelection == "scissors" && computerSelection == "paper") || 
+  (playerSelection == "paper" && computerSelection == "rock")
+  ){
+    return "Player";
+  } else {
+    return "Computer";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It's a Tie!"
+    } else if(result == "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else{
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));

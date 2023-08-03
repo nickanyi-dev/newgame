a07c5721abbe1ea59160e6991c69a823a630ddca
nickanyi-dev <nickanyi.dev@gmail.com>
2023-08-03 12:16:22 +0300

    add function to determine computer choice

---
+++ app.js
@@ -0,0 +1,9 @@
+function getComputerChoice(){
+    if(computerChoice <0.34){
+        computerChoice = "rock";
+    } else if(computerChoice <= 0.67){
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+}
